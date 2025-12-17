a3d63ff9271930c44549f340c4f3934dad747356
kunchala1909 <144514521+kunchala1909@users.noreply.github.com>
2025-12-17 17:31:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,2 @@
-body {
-  background-color: #0f172a;
-  color: #e5e7eb;
-  font-family: Arial, sans-serif;
-  text-align: center;
-  padding-top: 60px;
-}
+console.log("DevOps Portfolio Loaded Successfully");
 
-.fade {
-  animation: fadeIn 2s ease-in;
-}
-
-.slide {
-  animation: slideIn 1.5s ease-in;
-}
-
-@keyframes fadeIn {
-  from { opacity: 0; }
-  to { opacity: 1; }
-}
-
-@keyframes slideIn {
-  from { transform: translateY(30px); opacity: 0; }
-  to { transform: translateY(0); opacity: 1; }
-}
